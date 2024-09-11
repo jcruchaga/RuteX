d8f6636a2a7c8da4299a79d29ec6776a24c67a9c
franja <66020188+jcruchaga@users.noreply.github.com>
2024-09-11 15:23:38 -0300

    RuteX v05.04

--- vendor/rutex/engine.php
+++ vendor/rutex/engine.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="static/css/style.css">
-    <title>RuteX v05.03</title>
+    <title>RuteX v05.04</title>
 </head>
 <body>
-    <h1>Powered by Rutex v05.03</h1><hr>
+    <h1>Powered by Rutex v05.04</h1><hr>
     <h3>RutexCorePath=<?=__DIR__?><br><br>
     <a href="/">Return to Home Page</a>
 </body>
